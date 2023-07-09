e0cb0823fa3cd55a0422020e6454d9effd572382
bagley2014 <dbagley@harding.edu>
2023-07-09 02:32:21 -0700

    Add initial card component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script lang="ts">
+	import Card from '@components/Card.svelte'
+	import rawCardData from '@assets/marvel-snap-cards/data.json'
+	import type { CardData } from '@/generated/CardData'
+
+	const cardData = rawCardData as { [id: string]: CardData }
+	let card = cardData.Angel
+</script>
+
 <h1>Hello and welcome to my site!</h1>
 <a href="/about">About my site</a>
+<br />
+<Card {card} />
