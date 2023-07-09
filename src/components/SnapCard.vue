fb6787263935c9203a8c074a8edca2e582372d47
bagley2014 <dbagley@harding.edu>
2023-07-08 18:24:48 -0700

    Initial, temporary, Vue updates

---
+++ src/components/SnapCard.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+const props = defineProps(['card'])
+
+const imgUrl = computed(() => {
+	console.log(new URL(`../assets/marvel-snap-cards/${props.card.imageName}`, import.meta.url))
+	console.log(`../assets/marvel-snap-cards/${props.card.imageName}`)
+	return new URL(`../assets/marvel-snap-cards/${props.card.imageName}`, import.meta.url).href
+})
+</script>
+
+<template>
+	<img :id="`card-${card.id}`" :src="imgUrl" />
+</template>
