7d977815f990612d7fe455a8ba37290018ebb79f
bagley2014 <dbagley@harding.edu>
2023-07-10 06:08:16 -0700

    Add initial calculator component

---
+++ src/components/Calculator.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import Textfield from '@smui/textfield'
+	import Paper, { Title, Subtitle, Content } from '@smui/paper'
+	import { debounce } from 'radash'
+	import type { CardData } from '@generated/models'
+
+	export let deck: CardData[]
+	let value = ''
+	let subtitle = 'Default Subtitle'
+	let content = 'Default content'
+
+	const onInputChanged = debounce({ delay: 100 }, _event => {
+		content = deck.toString()
+		subtitle = value
+	})
+</script>
+
+<Textfield textarea bind:value on:input={onInputChanged} style="width: 96%; min-height:24em; margin-top:6px;" label="Calculator Script" />
+
+<Paper variant="unelevated">
+	<Title>Calculator Results</Title>
+	<Subtitle>{subtitle}</Subtitle>
+	<Content>{content}</Content>
+</Paper>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,13 @@
 	import type { CardData } from '@generated/models'
 	import Deck from '@components/Deck.svelte'
 	import DeckImporter from '@components/DeckImporter.svelte'
+	import Calculator from '@components/Calculator.svelte'
 
 	let cards: CardData[] = []
 </script>
 
-<h1>Hello and welcome to my site!</h1>
+<h3>Hello and welcome to my site!</h3>
 
 <DeckImporter bind:importedDeck={cards} />
 <Deck {cards} />
+<Calculator deck={cards} />
