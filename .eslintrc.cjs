afaad82fe02af03e91ace1802b106573e561e685
bagley2014 <dbagley@harding.edu>
2023-07-10 01:36:55 -0700

    Run prettier
    And update the prettier ignore to keep from touching things that aren't "true" source files.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,33 +1,28 @@
 module.exports = {
 	root: true,
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:svelte/recommended',
-		'prettier'
-	],
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:svelte/recommended', 'prettier'],
 	parser: '@typescript-eslint/parser',
 	plugins: ['@typescript-eslint'],
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020,
-		extraFileExtensions: ['.svelte']
+		extraFileExtensions: ['.svelte'],
 	},
 	env: {
 		browser: true,
 		es2017: true,
-		node: true
+		node: true,
 	},
 	overrides: [
 		{
 			files: ['*.svelte'],
 			parser: 'svelte-eslint-parser',
 			parserOptions: {
-				parser: '@typescript-eslint/parser'
-			}
-		}
+				parser: '@typescript-eslint/parser',
+			},
+		},
 	],
 	rules: {
 		'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
 	},
-};
+}
