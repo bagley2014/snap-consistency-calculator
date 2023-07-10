23052a49d84710435a2d9d0752235a26b31faba0
bagley2014 <dbagley@harding.edu>
2023-07-10 07:41:52 -0700

    Add command to get test coverage

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
-import { sveltekit } from '@sveltejs/kit/vite'
+import { configDefaults } from 'vitest/config'
 import { defineConfig } from 'vite'
+import { sveltekit } from '@sveltejs/kit/vite'
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	test: {
+		coverage: {
+			exclude: [...configDefaults.exclude, 'src/generated/**'],
+		},
+	},
 })
